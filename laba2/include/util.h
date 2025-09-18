ff98dd1b89d989a85151ef4f5924f67b39d5a588
Yukio11111 <royalemoyy@gmail.com>
2025-09-18 17:08:33 +0300

    Add Hello World project with CMake

---
+++ laba2/include/util.h
@@ -0,0 +1,8 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <string>
+
+std::string getGreeting();
+
+#endif // UTIL_H
---
+++ laba2/src/util.cpp
@@ -0,0 +1,6 @@
+#include "util.h"
+#include <string>
+
+std::string getGreeting() {
+    return "Hello, World!";
+}
