9ae02b04ea0a57ed362cb4f273f4e7fc18dce29e
Yukio11111 <royalemoyy@gmail.com>
2025-11-03 15:27:51 +0000

    hui

---
+++ laba6/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /workspaces/my-repository/laba6
+# Build directory: /workspaces/my-repository/laba6/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(IteratorTests "/workspaces/my-repository/laba6/build/run_tests")
+set_tests_properties(IteratorTests PROPERTIES  _BACKTRACE_TRIPLES "/workspaces/my-repository/laba6/CMakeLists.txt;55;add_test;/workspaces/my-repository/laba6/CMakeLists.txt;0;")
